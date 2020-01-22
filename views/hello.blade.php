f810a69fabb8c1249d12c7cdcdeb9d18b9f1c3d0
おぬき <31175068+yokenzan@users.noreply.github.com>
2020-01-22 17:51:29 +0900

    rename sample blade view file

--- views/hello.blade.php
+++ views/hello.blade.php
@@ -2,10 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>{{ $message }}</title>
+    <title>Minimal Config To Use Blade Template Engine</title>
 </head>
 <body>
-    
+    <p>{{ $message }}</p>
 </body>
 </html>
 
