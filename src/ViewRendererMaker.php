9824c5cee81206c3a3db5b003233ae0765b6691b
onuki <31175068+yokenzan@users.noreply.github.com>
2020-04-18 21:53:38 +0900

    add PHP and File compiler engines

--- src/ViewRendererMaker.php
+++ src/ViewRendererMaker.php
@@ -2,33 +2,31 @@
 
 namespace MinimalBlade;
 
-use Illuminate\Contracts\View\Engine;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\View\Compilers\BladeCompiler;
 use Illuminate\View\Engines\CompilerEngine;
 use Illuminate\View\Engines\EngineResolver;
+use Illuminate\View\Engines\FileEngine;
+use Illuminate\View\Engines\PhpEngine;
 use Illuminate\View\Factory;
 use Illuminate\View\FileViewFinder;
 
 /**
  * The class which makes ViewRenderer instance by completing dependencies with default Illuminate objects.
+ *
+ * @link https://github.com/illuminate/view/blob/master/ViewServiceProvider.php
  */
 class ViewRendererMaker
 {
     /**
      * default directory path to store cached Blade view files in
      */
-    private const CACHE_PATH      = 'cache';
+    private const CACHE_PATH = 'cache';
     /**
      * default directory paths to find Blade view files in
      */
-    private const VIEW_PATHS      = ['views'];
-    /**
-     * default file extensions to be treated as Blade view file
-     */
-    private const VIEW_EXTENSIONS = ['blade.php'];
-
+    private const VIEW_PATHS = ['views'];
 
     /**
      * @var string   directory path to store cached Blade view files in
@@ -39,22 +37,20 @@ class ViewRendererMaker
      */
     private $viewPaths;
     /**
-     * @var string[] file extensions to be treated as Blade view file
+     * @var string[]|null file extensions to be treated as Blade view file
      */
     private $viewExtensions;
 
-
     /**
-     * @param string    $cachePath       directory path to store cached Blade view files in
-     * @param string[]  $viewPaths       directory paths to find Blade view files in
-     * @param string[]  $viewExtensions  file extensions to be treated as Blade view file
+     * @param string         $cachePath       directory path to store cached Blade view files in
+     * @param string[]       $viewPaths       directory paths to find Blade view files in
+     * @param string[]|null  $viewExtensions  file extensions to be treated as Blade view file
      */
     public function __construct(
-        string  $cachePath      = self::CACHE_PATH,
-        array   $viewPaths      = self::VIEW_PATHS,
-        array   $viewExtensions = self::VIEW_EXTENSIONS
-    )
-    {
+        string  $cachePath = self::CACHE_PATH,
+        array   $viewPaths = self::VIEW_PATHS,
+        ?array  $viewExtensions = null
+    ) {
         $this->cachePath      = $cachePath;
         $this->viewPaths      = $viewPaths;
         $this->viewExtensions = $viewExtensions;
@@ -87,14 +83,17 @@ class ViewRendererMaker
          * apply configuration values
          */
 
-        if(isset($config['cachePath']))
-            $this->cachePath      = $config['cache_path'];
+        if (isset($config['cachePath'])) {
+            $this->cachePath = $config['cache_path'];
+        }
 
-        if(isset($config['view_paths']))
-            $this->viewPaths      = $config['view_paths'];
+        if (isset($config['view_paths'])) {
+            $this->viewPaths = $config['view_paths'];
+        }
 
-        if(isset($config['view_extensions']))
+        if (isset($config['view_extensions'])) {
             $this->viewExtensions = $config['view_extensions'];
+        }
 
         /*
          * make renderer
@@ -104,41 +103,28 @@ class ViewRendererMaker
     }
 
     /**
-     * Makes FileViewFinder instance.
+     * Make FileViewFinder instance.
      *
-     * @param string[] $viewPaths
-     * @param string[] $viewExtensions
+     * @param string[]      $viewPaths
+     * @param string[]|null $viewExtensions
      */
-    private function makeFileViewFinder(array $viewPaths, array $viewExtensions): FileViewFinder
+    private function makeFileViewFinder(array $viewPaths, ?array $viewExtensions): FileViewFinder
     {
         return new FileViewFinder($this->makeFileSystem(), $viewPaths, $viewExtensions);
     }
 
     /**
-     * Makes EngineResolver instance.
-     *
-     * @param string $cachePath
+     * Make EngineResolver instance.
      */
-    private function makeEngineResolver(string $cachePath): EngineResolver
+    private function makeEngineResolver(): EngineResolver
     {
         $resolver = new EngineResolver();
-        $resolver->register(
-            'blade',
-            function () use ($cachePath) {
-                return $this->makeCompilerEngine($cachePath);
-            }
-        );
-        return $resolver;
-    }
 
-    /**
-     * Makes Engine instance.
-     *
-     * @param string $cachePath
-     */
-    private function makeCompilerEngine(string $cachePath): Engine
-    {
-        return new CompilerEngine(new BladeCompiler($this->makeFileSystem(), $cachePath));
+        $this->registerFileEngine($resolver);
+        $this->registerPhpEngine($resolver);
+        $this->registerBladeEngine($resolver);
+
+        return $resolver;
     }
 
     /**
@@ -147,5 +133,47 @@ class ViewRendererMaker
     private function makeFileSystem(): Filesystem
     {
         return new Filesystem();
+    }
+
+    /**
+     * Register the file engine implementation.
+     *
+     * @param  \Illuminate\View\Engines\EngineResolver  $resolver
+     * @return void
+     * @link https://github.com/illuminate/view/blob/master/ViewServiceProvider.php
+     */
+    private function registerFileEngine($resolver)
+    {
+        $resolver->register('file', function () {
+            return new FileEngine();
+        });
+    }
+
+    /**
+     * Register the PHP engine implementation.
+     *
+     * @param  \Illuminate\View\Engines\EngineResolver  $resolver
+     * @return void
+     * @link https://github.com/illuminate/view/blob/master/ViewServiceProvider.php
+     */
+    private function registerPhpEngine($resolver)
+    {
+        $resolver->register('php', function () {
+            return new PhpEngine();
+        });
+    }
+
+    /**
+     * Register the Blade engine implementation.
+     *
+     * @param  \Illuminate\View\Engines\EngineResolver  $resolver
+     * @return void
+     * @link https://github.com/illuminate/view/blob/master/ViewServiceProvider.php
+     */
+    private function registerBladeEngine($resolver)
+    {
+        $resolver->register('blade', function () {
+            return new CompilerEngine(new BladeCompiler($this->makeFileSystem(), $this->cachePath));
+        });
     }
 }
